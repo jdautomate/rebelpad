f4a04d0eb09528811913e7f782d624e52e3db66e
jdautomate <jd@rebelnotes.us>
2019-05-13 19:28:27 -0700

    Personal directories added to home/name/

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -11,6 +11,18 @@ node default {
     opt_misc => ['hlsearch','showcmd','showmatch','ignorecase','smartcase','incsearch','autowrite','hidden','number'],
   }
 
+  file { '/home/justin/plex':
+    ensure => 'directory',
+  }
+
+  file { '/home/justin/keys':
+    ensure => 'directory',
+  }
+
+  file { '/home/justin/infra':
+    ensure => 'directory',
+  }
+
   archive { '/tmp/hyper_3.0.2_amd64.deb':
     source => 'https://releases.hyper.is/download/deb',
     ensure => 'present',
@@ -28,5 +40,4 @@ node default {
   package {[ 'git', 'wget', 'htop', 'x2goclient' ]:
     ensure => 'installed',
   }
-  # apt htop, etc.
 }
